7d62019def6fad3f873d009f052449b4c517b8ed
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-12 12:36:22 +0200

    added source-contenful and .env files

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,20 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `lh2dzm21y4wy`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
