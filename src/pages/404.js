e37c318c458f85f6b3a6059bd09224d725f9fcc5
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-17 15:58:26 +0200

    404 updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
+import Layout from "../components/Layout"
+
+const ErrorWrapper = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  a {
+    text-decoration: none;
+    color: #f2b850;
+  }
+`
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h2>wrong page brah</h2>
-    </div>
+    <Layout>
+      <ErrorWrapper>
+        <h2>Woah, this page does not exist!</h2>
+        <Link to="/">Click here to get back to safety!</Link>
+      </ErrorWrapper>
+    </Layout>
   )
 }
 
