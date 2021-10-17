95b60db72b07abf12451709f7a1cf04b5bd4d0bf
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-17 09:47:09 +0200

    menu section finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import "normalize.css"
 import HomeSection from "../components/HomeSection"
+import Menu from "../components/Menu"
 
 export default function Home() {
   return (
     <Layout>
       <HomeSection />
+      <Menu />
     </Layout>
   )
 }
