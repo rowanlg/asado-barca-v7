e765380f736ce7b1849b8aabb0fc9ddbc729da59
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-18 14:53:48 +0200

    moved iframe to useEffect hook

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import BgImages from "./BgImages"
 
@@ -38,11 +38,23 @@ const Section = styled.section`
 `
 
 const Contact = () => {
+  useEffect(() => {
+    const iframeTag = document.createElement("iframe")
+
+    iframeTag.src =
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2993.125441596119!2d2.147384715425964!3d41.39308207926357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a32519083271%3A0xbac256f23168a4d6!2sAsado%20Hamburguesas!5e0!3m2!1sen!2snl!4v1634480436040!5m2!1sen!2snl"
+    iframeTag.async = true
+
+    document.getElementById("google-map").appendChild(iframeTag)
+    return () => {
+      document.getElementById("google-map").removeChild(iframeTag)
+    }
+  }, [])
   return (
     <BgImages imgId={1}>
       <Section id="contact">
-        <div className="map-section">
-          <iframe
+        <div className="map-section" id="google-map">
+          {/* <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2993.125441596119!2d2.147384715425964!3d41.39308207926357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a32519083271%3A0xbac256f23168a4d6!2sAsado%20Hamburguesas!5e0!3m2!1sen!2snl!4v1634480436040!5m2!1sen!2snl"
             frameBorder="0"
             style={{ border: 0 }}
@@ -50,7 +62,7 @@ const Contact = () => {
             aria-hidden="false"
             tabIndex="0"
             title="Location"
-          />
+          /> */}
         </div>
         <div className="info">
           <h2>Asado Barcelona</h2>
