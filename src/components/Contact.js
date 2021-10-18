d2f8688bc2ee4243e5a394eda1de579517a7c1b0
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-18 15:28:22 +0200

    useOnScreen hook implemented for google maps v2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef, useOnScreen } from "react"
+import React, { useRef, useState, useEffect } from "react"
 import styled from "styled-components"
 import BgImages from "./BgImages"
 
@@ -37,6 +37,30 @@ const Section = styled.section`
   }
 `
 
+// Hook
+function useOnScreen(ref, rootMargin = "0px") {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState(false)
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        setIntersecting(entry.isIntersecting)
+      },
+      {
+        rootMargin,
+      }
+    )
+    if (ref.current) {
+      observer.observe(ref.current)
+    }
+    return () => {
+      observer.unobserve(ref.current)
+    }
+  }, []) // Empty array ensures that effect is only run on mount and unmount
+  return isIntersecting
+}
+
 const Contact = () => {
   const ThirdPartyIframe = () => {
     return (
@@ -54,28 +78,12 @@ const Contact = () => {
 
   const ref = useRef()
   const onScreen = useOnScreen(ref, "0px")
-  let showWidget
-  if (onScreen) {
-    showWidget = true
-  }
 
-  // useEffect(() => {
-  //   const iframeTag = document.createElement("iframe")
-
-  //   iframeTag.src =
-  //     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2993.125441596119!2d2.147384715425964!3d41.39308207926357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a4a32519083271%3A0xbac256f23168a4d6!2sAsado%20Hamburguesas!5e0!3m2!1sen!2snl!4v1634480436040!5m2!1sen!2snl"
-  //   iframeTag.async = true
-
-  //   document.getElementById("google-map").appendChild(iframeTag)
-  //   return () => {
-  //     document.getElementById("google-map").removeChild(iframeTag)
-  //   }
-  // }, [])
   return (
     <BgImages imgId={1}>
       <Section id="contact">
         <div className="map-section" id="google-map" ref={ref}>
-          {showWidget && <ThirdPartyIframe />}
+          {onScreen ? <ThirdPartyIframe /> : <div />}
         </div>
         <div className="info">
           <h2>Asado Barcelona</h2>
