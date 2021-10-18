b03a73f0203059bb546545309f0d1aa90f0fa40c
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-18 13:11:02 +0200

    added gatsby-plugin-netlify for http2 config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-preact`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
