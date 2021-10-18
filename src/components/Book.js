989257a28109f2295a0cf00e2e65a8a59b7dd3a6
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-18 15:41:56 +0200

    added scroll load to opentable too to imporve performance of site load

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useEffect } from "react"
+import { useEffect, useState, useRef } from "react"
 import styled from "styled-components"
 import BgImages from "./BgImages"
 
@@ -18,23 +18,59 @@ const Section = styled.section`
   }
 `
 
-const Book = () => {
+// Hook
+function useOnScreen(ref, rootMargin = "0px") {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState(false)
   useEffect(() => {
-    const scriptTag = document.createElement("script")
-
-    scriptTag.src =
-      "//www.opentable.com/widget/reservation/loader?rid=237078&type=standard&theme=standard&iframe=true&domain=com&lang=es-MX&newtab=false&ot_source=Restaurant%20website"
-    scriptTag.async = true
-
-    document.getElementById("open-table").appendChild(scriptTag)
-    return () => {
-      document.getElementById("open-table").removeChild(scriptTag)
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        // Update our state when observer callback fires
+        setIntersecting(entry.isIntersecting)
+      },
+      {
+        rootMargin,
+      }
+    )
+    if (ref.current) {
+      observer.observe(ref.current)
     }
-  }, [])
+    return () => {
+      observer.unobserve(ref.current)
+    }
+  }, []) // Empty array ensures that effect is only run on mount and unmount
+  return isIntersecting
+}
+
+const Book = () => {
+  const ref = useRef()
+  const onScreen = useOnScreen(ref, "0px")
+
+  // useEffect(() => {
+  //   const scriptTag = document.createElement("script")
+
+  //   scriptTag.src =
+  //     "//www.opentable.com/widget/reservation/loader?rid=237078&type=standard&theme=standard&iframe=true&domain=com&lang=es-MX&newtab=false&ot_source=Restaurant%20website"
+  //   scriptTag.async = true
+
+  //   document.getElementById("open-table").appendChild(scriptTag)
+  //   return () => {
+  //     document.getElementById("open-table").removeChild(scriptTag)
+  //   }
+  // }, [])
+
   return (
     <BgImages imgId={0}>
-      <Section id="book">
-        <div id="open-table" />
+      <Section id="book" ref={ref}>
+        {/* <div id="open-table" />  */}
+        {onScreen ? (
+          <script
+            type="text/javascript"
+            src="//www.opentable.com/widget/reservation/loader?rid=237078&type=standard&theme=standard&iframe=true&domain=com&lang=es-MX&newtab=false&ot_source=Restaurant%20website"
+          ></script>
+        ) : (
+          <script />
+        )}
       </Section>
     </BgImages>
   )
