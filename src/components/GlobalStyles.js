a00728ae991a449882468007bd1283427d45fdfc
Rowan Gordon <rowan@rowangordon.co.uk>
2021-10-19 09:39:57 +0200

    font-display v2

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,12 +3,10 @@ import { createGlobalStyle } from "styled-components"
 const GlobalStyles = createGlobalStyle`
   body {
     font-family: Dker;
-    font-display: swap;
     color: #fafafa;
     background-color: black;
     p {
       font-family: Abel;
-      font-display: swap;
       font-size: 1.2rem;
     }
   }
