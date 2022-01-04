186ff988ebc385854473789f25fff2de73763891
Rowan Gordon <rowan@rowangordon.co.uk>
2022-01-04 18:03:36 +0530

    bg image numbering changed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -83,7 +83,7 @@ const Contact = () => {
   }
 
   return (
-    <BgImages imgId={0}>
+    <BgImages imgId={3}>
       <Section id="contact">
         <div className="map-section" id="google-map" ref={container}>
           {render && <ThirdPartyIframe />}
